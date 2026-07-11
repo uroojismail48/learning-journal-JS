873358e47291c2afd8d32240963bbe1f6d2a5498
urooj <uroojismail48@gmail.com>
2026-07-12 04:04:31 +0500

    Age-Calculator Added

---
+++ JS-Mini-projects/Age-Calculator/Age.js
@@ -0,0 +1,17 @@
+let box = document.getElementById("input") 
+let h1 =  document.getElementById("h1") 
+function go(){
+ 
+    h1.style.color = "green"
+
+    let birth = new Date(box.value)
+    let today = new Date()
+   
+    let age = today.getFullYear() - birth.getFullYear();
+    let month = today.getMonth()  - birth.getMonth()
+
+    if(month < 0 || (month === 0 && today.getDate() < birth.getDate()) ){
+        age--
+    }
+       h1.innerHTML = `Your Age is ${age} Years`;
+}
