ae2ad3ba77c1d9f49b72879a8eb73ad5a137e3e7
urooj <uroojismail48@gmail.com>
2026-07-14 02:23:43 +0500

    QR_Generator-Added

--- JS-Mini-projects/QR-Code-generator/QR.js
+++ JS-Mini-projects/QR-Code-generator/QR.js
@@ -2,7 +2,7 @@
             const para = document.querySelector(".para")
                 const img = document.querySelector("#img")
     const input = document.querySelector("#input")
-    const word = input.value.trim().split(/\s+/);
+
     function qrcode (){
         
          if (input.value.trim() === ""){
@@ -13,7 +13,7 @@
         }else if (input.value === input.value){
             para.innerHTML = `"${input.value} QR Generated Successfully";`
             para.style.color = "green";
-        }
+         }
           img.src = "https://api.qrserver.com/v1/create-qr-code/?size=150x150&data=" + encodeURIComponent(input.value);
         box.classList.add("show-img")
        
