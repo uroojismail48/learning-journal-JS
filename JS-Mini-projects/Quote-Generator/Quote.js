f6b49080e1235fff61de8721caee316096ea30c6
urooj <uroojismail48@gmail.com>
2026-07-14 03:14:09 +0500

    Quote-Generator-Added

---
+++ JS-Mini-projects/Quote-Generator/Quote.js
@@ -0,0 +1,22 @@
+
+const bb = document.querySelector(".bb")
+const aa = document.querySelector(".aa")
+const btn = document.getElementById("btn")
+const api = " https://dummyjson.com/quotes/random"
+
+function getQuote() {
+    try {
+        fetch(api).then(res => res.json()).then(data => {
+            aa.innerHTML = `${data.author} says`
+            bb.innerHTML = `${data.quote} `
+        }
+        )
+    }catch(error)
+    {
+ quote.innerText = "Failed to load quote.";
+        author.innerText = "";
+        console.error(error);
+    }
+}
+
+btn.addEventListener("click" , getQuote )
